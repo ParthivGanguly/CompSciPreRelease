c36b53a9be468ef9c096165fe260aaef90ac89ae
Parthiv Ganguly <parthiv9384@gmail.com>
2018-02-24 10:16:37 +0530

    First Commit

---
+++ tasks.py
@@ -0,0 +1,36 @@
+YIELDTHRESHOLD = 12
+DAYSOFWEEK = 4
+CowPopulation = 0
+while CowPopulation == 0 or CowPopulation > 999:
+    CowPopulation = int(input("Enter number of cows in the herd: "))
+    if CowPopulation == 0 or CowPopulation > 999:
+        print("Cow Population must be between 1 to 999 (inclusive)")
+yields = []
+for i in range(CowPopulation):
+    yields.append([])
+for day in range(1, 8):
+    dayOver = False
+    while not dayOver:
+        cowID = ""
+        while len(cowID) > 3 or len(cowID) < 3:
+            cowID = input("Enter Cow ID: ")
+            if len(cowID) < 3 or len(cowID) > 3:
+                print("Cow ID must be exactly 3 digits long")
+        litres = 0.0
+        while litres <= 0.0:
+            litres = float(input("Enter litres of milk: "))
+            if litres <= 0.0:
+                print("Enter positive number")
+        for cow in yields:
+            if cowID in cow:
+                if len(cow) == day:
+                    cow.append(litres)
+                else:
+                    cow[day] += litres
+            elif len(cow) == 0:
+                cow.append(cowID)
+                cow.append(litres)
+                break
+        if input("Is the day over ? Yes (y) or No (Any other key): ") == "y":
+            dayOver = True
+print(yields)
